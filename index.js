5beb4d56a5b2d053c434278832e95baf32f075ff
anastafish <anastafish24@gmail.com>
2023-05-10 22:37:10 +0300

    URL Shortener Microservice

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const urlSchema = new mongoose.Schema({
   original_url:String,
-  short_url:String
+  short_url:Number
 })
 
 const Urls = mongoose.model('Urls', urlSchema)
@@ -50,36 +50,10 @@ app.post('/api/shorturl', (req, res) => {
     }
   }
   )
-  // dns.lookup(req.body.url.slice(8), err => {
-  //   if (err){
-  //     res.json({ error: 'invalid url' })
-  //   }
-  //   else if (req.body.url.slice(0,7) === 'http://') {
-  //     Urls.count({}).then(data => {
-  //       const newUrl = new Urls({
-  //         original_url: req.body.url,
-  //         short_url: data
-  //       })
-    
-  //       newUrl.save().then(data => {
-  //         res.json({
-  //           original_url: data.original_url,
-  //           short_url: data.short_url
-  //         })
-  //       }).catch(err => {
-  //         console.log(err)
-  //       })
-    
-  //     }).catch(err => console.log(err))   
-  //   }
-  //   else {
-  //     res.json({ error: 'invalid url' })
-  //   }
-  // })   
 })
 
 app.get('/api/shorturl/:short_url', (req, res) => {
-  Urls.findOne({short_url:req.params.short_url}).then(data => {
+  Urls.findOne({short_url: +req.params.short_url}).then(data => {
     res.redirect(data.original_url)
   }).catch(err => console.log(err))
   })
