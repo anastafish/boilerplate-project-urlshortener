b93da761177e46a177dcb160b833c946fab1158a
anastafish <anastafish24@gmail.com>
2023-05-10 16:29:53 +0300

    URL Shortener Microservice

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const dns = require('node:dns');
+mongoose.connect(process.env.mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const urlSchema = new mongoose.Schema({
+  original_url:String,
+  short_url:Number
+})
+
+const Urls = mongoose.model('Urls', urlSchema)
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +20,40 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.post('/api/shorturl', (req, res) => {
+  dns.lookup(req.body.url, err => {
+    if (err){
+      res.json({ error: 'invalid url' })
+    }
+    else {
+      Urls.count({}).then(data => {
+        const newUrl = new Urls({
+          original_url: req.body.url,
+          short_url: data
+        })
+    
+        newUrl.save().then(data => {
+          res.json({
+            original_url: data.original_url,
+            short_url: data.short_url
+          })
+        }).catch(err => {
+          console.log(err)
+        })
+    
+      }).catch(err => console.log(err))   
+    }
+  })   
+})
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  Urls.findOne({short_url:req.params.short_url}).then(data => {
+    res.redirect(`https://${data.original_url}`)
+  }).catch(err => console.log(err))
+  })
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
