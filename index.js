1607ef89087eed14d43463a35cb050e2bf583bbb
anastafish <anastafish24@gmail.com>
2023-05-10 16:46:03 +0300

    URL Shortener Microservice

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const dns = require('node:dns');
-mongoose.connect(process.env.mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const urlSchema = new mongoose.Schema({
   original_url:String,
